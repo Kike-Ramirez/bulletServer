c1ce3bb865791a10b20c31d80e4d4abf8b6f2002
kike-ramirez <kike.ramirez@fullsix.com>
2017-07-22 17:57:16 +0200

    changes

--- bulletServer.py
+++ bulletServer.py
@@ -3,18 +3,25 @@ import wget
 import time
 import shutil
 
-r = requests.get('http://10.42.0.102:8000/start')
-print 'bulletCam started...'
-print''
+
+
+for counter in range(42):
+	r = requests.get('http://10.42.0.' + str(counter + 100) + ':8000/start')
+	print 'bulletCam started...' + str(counter)
+
+
 time.sleep(4)
 
-r = requests.get('http://10.42.0.102:8000/stop')
-print 'bulletCam stopped...'
 
+for counter in range(42):
+	r = requests.get('http://10.42.0.' + str(counter + 100) + ':8000/stop')
+	print 'bulletCam stopped...' + str(counter)
 
-url = 'http://10.42.0.102:8000/get/10'
-response = requests.get(url, stream=True)
-print 'trying to get image'
-with open('10.png', 'wb') as out_file:
-    shutil.copyfileobj(response.raw, out_file)
-del response
+for counter in range(42):
+	r = requests.get('http://10.42.0.' + str(counter + 100) + ':8000/get/' + str(counter))
+	response = requests.get(url, stream=True)
+	print 'trying to get image' 
+	fileName = 'picture_' + str(counter) + '.png'
+	with open(fileName, 'wb') as out_file:
+	    shutil.copyfileobj(response.raw, out_file)
+	del response
